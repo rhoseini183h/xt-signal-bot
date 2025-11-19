a3328d9c558191b5e49ae2ccebda58c9da5f0aa1
rhoseini183h <rhoseini183h@gmail.com>
2025-11-19 10:14:22 +0330

    Add Telegram bot initialization and message handler

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "ربات فعال است ✔️");
+});
+
